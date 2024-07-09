7982c638fae4e297b58d4c981b98316f9074915d
kang-minjune <bkokmj0327@gmail.com>
2024-07-09 17:33:49 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminDashBoard from "./pages/AdminDashBoard";
 import Login from './components/login/Login';
@@ -13,17 +12,6 @@ function App() {
       </Routes>
       {/* <ChatApp/>이 여기 들어갈 예정 */}
     </BrowserRouter>
-=======
-import Calendar from "../src/pages/Calendar";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>hello Youth!</h1>
-      <h2>Dev Branch Test</h2>
-      <Calendar></Calendar>
-    </div>
->>>>>>> Stashed changes
   );
 }
 
