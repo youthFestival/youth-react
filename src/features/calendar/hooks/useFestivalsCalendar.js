9964170d16d418d0c4fa93ed5dff55bfb579f8ec
moble <dudwls5674@naver.com>
2024-07-09 14:43:57 +0900

    df

---
+++ src/features/calendar/hooks/useFestivalsCalendar.js
@@ -0,0 +1,24 @@
+import {useState, useEffect} from 'react';
+import {fetchFestivalCalendar} from "../services/festivalCalendarService";
+
+const useFestivals = (year, month) => {
+    const [festivals, setFestivals] = useState([]);
+
+    useEffect(() => {
+        const getFestivals = async() => {
+            try {
+                const data = await fetchFestivalCalendar(year, month);
+                setFestivals(data);
+            }
+            catch(error) {
+                console.error(error);
+            }
+        };
+
+        getFestivals();
+    }, [year, month]);
+
+    return festivals;
+};
+
+export default useFestivals;
--- src/features/calendar/services/festivalCalendarService.js
+++ src/features/calendar/services/festivalCalendarService.js
@@ -1,1 +1,6 @@
 import axios from "axios";
+// 축제 정보 가져오는 api 호출 부분
+
+export const fetchFestivalCalendar = async(year, month) => {
+
+};
