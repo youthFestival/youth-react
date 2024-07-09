ff3e1a4cda53742bad0fb4c1d476b6f599555e79
kang-minjune <bkokmj0327@gmail.com>
2024-07-09 17:32:19 +0900

    폰트 CSS 작성

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-
+import './login.css'
 /**
  * 로그인 화면
  * @returns
  */
 const Login = () => {
+
+    
   return (
-    <div>
-      <div className="loginAll"></div>
-    </div>
+      <div className="loginAll">
+  
+            {/* 로그인 메인 폼 */}
+            <div className='loginContentsForm'>
+                <div className='loginLogoDiv'>
+                    <span className='loginYouthLogo'>Youth!</span>
+                </div>
+            </div>
+        </div>
   );
 };
 
