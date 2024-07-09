2868466f338fc7e4163f0e157b68909e01a8e364
kang-minjune <bkokmj0327@gmail.com>
2024-07-09 18:20:56 +0900

    파일 경로 수정

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import '../styles/login.css';
+/**
+ * 로그인 화면
+ * @returns
+ */
+const Login = () => {
+
+    
+  return (
+      <div className='login-form'>
+
+         <div className='login-container'>
+            {/* 로그인 메인 폼 */}
+            <div className='login-contents'>
+                <div className='login-logo-box'>
+                    <span className='login-youth'>Youth!</span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  );
+};
+
+export default Login;
