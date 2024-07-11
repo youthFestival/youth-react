67a2e34aaa40ea7fa3e60022ffbb60f94abb24ed
서윤오 <seo0jjjjj@gmail.com>
2024-07-11 21:03:27 +0900

    add dashborad side components

---
+++ src/features/dashboard/components/Header.jsx
@@ -0,0 +1,5 @@
+function header() {
+    return (<div>Header</div>);
+}
+
+export default header;
---
+++ src/features/dashboard/components/TableContent.jsx
@@ -0,0 +1,9 @@
+function TableContent()) {
+    return (
+        <div>
+            <h1>Table Content</h1>
+        </div>
+    );
+}
+
+export default TableContent;
