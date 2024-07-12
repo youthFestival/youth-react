5c9f216ce7b2167654abe1b6c32e4366f2c9b80d
OrangeBoy98 <wogks119@gmail.com>
2024-07-12 18:49:14 +0900

    chore:feture 폴더 내 chatBot 외 다 삭제

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminDashBoard from "./pages/AdminDashBoard";
-import Login from './pages/Login';
-import Calendar from './pages/Calendar';
 import ChatBot from './features/chatBot/ChatBot';
 
 function App() {
@@ -10,9 +7,6 @@ function App() {
       <ChatBot />
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/admin" element={<AdminDashBoard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/calendar" element={<Calendar/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/features/authentication/index.js
+++
@@ -1,5 +0,0 @@
-// features > authentication>indexedDB.js
-export { default as LoginBtn } from './component/LoginBtn';
-export { default as KakaoAuthLogo} from './component/KakaoAuthLogo';
-export { default as NaverAuthLogo} from './component/NaverAuthLogo';
-export { default as GoogleAuthLogo} from './component/GoogleAuthLogo';
