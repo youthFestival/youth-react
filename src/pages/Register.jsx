ccd24da0a9e9c4a534ddc1b1e6c543a6bbc43843
kang-minjune <bkokmj0327@gmail.com>
2024-07-12 13:09:18 +0900

    회원가입

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import {
     AuthBtn,
     AuthLogo, 
     AuthInput,
-    AuthCheckbox
+    AuthCheckbox,
 } from '../features/authentication/index.js'
 
 import '../styles/register.css'
@@ -15,36 +15,70 @@ import '../styles/register.css'
 const Register = () => {
   return (
     <div className='register-container'>
+
       <div className='register-form'>
+
           <div className='register-contents'>
 
-                   <div className='register-youth'>
-                        
-                   </div>
-               
-                    <AuthLogo 
-                        logoDivName='naver-signup'
-                        logoHref='##2'
-                        logoSrc = "./icons/company/naverlogo.png" 
-                        logoAlt = "네이버 로그인 아이콘" 
-                        logoClassName='naverlogo'
-                    />
 
-                    <AuthLogo 
-                        logoDivName='kakao-signup'
-                        logoHref='##1'
-                        logoSrc="./icons/company/kakaologo.png" 
-                        logoAlt="카카오 로그인 아이콘" 
-                        logoClassName='kakaologo'
-                    />
+
+
+
+              <div className='register-youth'>
+                  
+              </div>
+
+
+
+               
+              <div className='register-logo-container'>
+
+
+                <div className='register-logo-form'>
+
+
+
+
+                    <div className='register-kakao'>
+                        <AuthLogo 
+                            logoSrc='./icons/company/kakaologo.png'
+                        />
+
+                        <AuthBtn
+                            btnClassName='kakao-signup-btn'
+                            btnText = '카카오로 가입'
+                         />
+                    </div>
+
+                    <div className='register-naver'>
+                        <AuthLogo 
+                            logoSrc='./icons/company/naverlogo.png'
+                        />
+                        
+                        <AuthBtn
+                            btnClassName='naver-signup-btn'
+                            btnText = '네이버로 가입'
+                         />
+                    </div>
+
+                    <div className='register-google'>
+                        <AuthLogo 
+                            logoSrc='./icons/company/googlelogo.png'
+                        />
+                        
+                        <AuthBtn
+                            btnClassName='google-signup-btn'
+                            btnText = '구글로 가입'
+                         />
+                    </div>
+
+
+
+
+                  </div>
+                  
+              </div>
                     
-                    <AuthLogo 
-                        logoDivName='google-signup'
-                        logoHref='##3'
-                        logoSrc="./icons/company/googlelogo.png" 
-                        logoAlt="구글 로그인 아이콘" 
-                        logoClassName='googlelogo'
-                    />
                         
           </div>
     
