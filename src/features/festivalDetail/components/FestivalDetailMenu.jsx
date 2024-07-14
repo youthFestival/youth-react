94aa36d9f8506cd3c30e112eea11fb86590667c6
moble <dudwls5674@naver.com>
2024-07-15 00:05:28 +0900

    축제 상세 css 수정

--- src/features/festivalDetail/components/FestivalDetailMenu.jsx
+++ src/features/festivalDetail/components/FestivalDetailMenu.jsx
@@ -5,9 +5,9 @@ const FestivalDetailMenu = ({ activeTab, onTabChange }) => {
     return (
         <div className="tabs-container">
             <div className="tabs">
-                <button onClick={() => onTabChange("details")} className={activeTab === "details" ? "active" : ""}>상세정보</button>
-                <button onClick={() => onTabChange("comments")} className={activeTab === "comments" ? "active" : ""}>코멘트<span className="festial-detail-count">6</span></button>
-                <button onClick={() => onTabChange("qna")} className={activeTab === "qna" ? "active" : ""}>QnA<span className="festial-detail-count">6</span></button>
+                <button onClick={() => onTabChange("details")} className={`tab-button ${activeTab === "details" ? "active" : ""}`}>상세정보</button>
+                <button onClick={() => onTabChange("comments")} className={`tab-button ${activeTab === "comments" ? "active" : ""}`}>코멘트<span className="festial-detail-count">6</span></button>
+                <button onClick={() => onTabChange("qna")} className={`tab-button ${activeTab === "qna" ? "active" : ""}`}>QnA<span className="festial-detail-count">6</span></button>
             </div>
         </div>
     );
