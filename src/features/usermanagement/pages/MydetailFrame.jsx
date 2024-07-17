04662afcdbe08c80e62fb0c6a974550f4c03d6bc
kang-minjune <bkokmj0327@gmail.com>
2024-07-17 13:49:13 +0900

    회원가입 입력 폼, 약관 동의 창 레이아웃 수정

---
+++ src/features/usermanagement/pages/MydetailFrame.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/mydetailframe.css'
+
+const MydetailFrame = () => {
+    return (
+        <div>
+            <button className='my-main-btn'>MY MAIN</button>
+        </div>
+    );
+};
+
+export default MydetailFrame;
