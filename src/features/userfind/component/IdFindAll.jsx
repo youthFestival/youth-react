4b116c22835b9017483afbbaad92551a30369c76
kang-minjune <bkokmj0327@gmail.com>
2024-07-18 01:14:39 +0900

    계정찾기 레이아웃 수정

--- src/features/userfind/component/IdFindAll.jsx
+++ src/features/userfind/component/IdFindAll.jsx
@@ -18,14 +18,14 @@ const IdFindAll = () => {
                 findType='radio' 
                 findText='이메일 인증으로 찾기' 
                 togleContents={<FindBtn findBtnText='이메일 인증' findNavLink='/id2'/>} 
-                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 인증으로 아이디를 찾을 수 있어요.'
+                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 이메일 인증으로 아이디를 찾을 수 있어요.'
             />
 
             <FindToggle 
                 findType='radio' 
                 findText='휴대폰 번호로 찾기' 
-                togleContents={<FindBtn findBtnText='휴대폰 번호로 찾기' findNavLink='/id3'/>} 
-                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 인증으로 아이디를 찾을 수 있어요.'
+                togleContents={<FindBtn findBtnText='휴대폰 인증' findNavLink='/id3'/>} 
+                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 번호로 아이디를 찾을 수 있어요.'
             />
         </div>
     );
