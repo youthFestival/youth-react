72e0bc5b8f56dab1f1188d0b579f1c15a69e2940
seo0jjjjj <seo0jjjjj@mail.com>
2024-07-17 11:14:44 +0900

    feat: formatDate함수 추가

--- src/utils/util.js
+++ src/utils/util.js
@@ -13,3 +13,18 @@ export function withParams(obj) {
 }
 
 
+/**
+ * Date를 지정된 형식으로 포맷팅하는 함수
+ * @param {String} dateStr - 포맷팅할 String 객체
+ * @returns {String} date - 포맷팅된 날짜 문자열
+ */
+export function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+
+    return `${year}년 ${month}월 ${day}일 ${hours}:${minutes}`;
+}
