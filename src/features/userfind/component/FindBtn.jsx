67c56f964db2bbfe37cdc9b19f028a777d8ae5d4
kang-minjune <bkokmj0327@gmail.com>
2024-07-18 10:27:50 +0900

    계정찾기 버튼 수정

--- src/features/userfind/component/FindBtn.jsx
+++ src/features/userfind/component/FindBtn.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import '../styles/findbtn.scss'
+import '../styles/findbtn.scss';
+
 /**
  * 계정찾기 토글 안 버튼 컴포넌트
  * @returns 
  */
+function FindBtn ({ findBtnText, findNavLink, findType }) {
+    const [finClickBtn, setFindClickBtn] = useState(true);
+    const [findIsContents, setFindIsContents] = useState(false);
 
-function FindBtn ({findBtnText,findNavLink}) {
+    const findClickBtnHandler = () => {
+        setFindClickBtn(!finClickBtn);
+        setFindIsContents(!findIsContents);
+    };
+
     return (
+        <div>
+
             <div className='find-btn'>
                 <NavLink to={findNavLink} className='navlink'>
                     <button className='realbtn'>
                         {findBtnText}
                     </button>
                 </NavLink>
-            </div>      
+            </div>
+        </div>
     );
-};
+}
 
 export default FindBtn;
