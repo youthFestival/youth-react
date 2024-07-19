d67f47aef9cca1c305c39aa3a4df0d822dc238f2
moble <dori022662@gmail.com>
2024-07-19 15:44:50 +0900

    풀

---
+++ src/components/ranking/Ranking.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ranking = () => {
+    return (
+        <div className="rank-container">
+            <dis className="rank-rank">
+                <p className="rank-"
+            </dis>
+        </div>
+    )
+}
