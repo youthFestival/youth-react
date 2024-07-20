0e6a6bc5107afc1fd9f1bcbb7e7e07242de54442
서윤오 <seo0jjjjj@gmail.com>
2024-07-21 01:07:05 +0900

    refactor : /home 으로 이동하는 navigator 수정 /home -> /

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { 
+import {
     IconBox,
     TicketBox,
     MyInfoIcon
@@ -10,78 +10,78 @@ const Mypage = () => {
     return (
         <div className='my-container'>
             <div className='my-contents'>
-                 
-                 <div className='my-management-box'>
+
+                <div className='my-management-box'>
                     <div className='my-profile-info'>
                         <div className='my-real-profile'>
 
-                            <img src="./icons/mypage/myprofileimg.png" alt="내 프로필 사진" className='my-profile-image'/>
+                            <img src="./icons/mypage/myprofileimg.png" alt="내 프로필 사진" className='my-profile-image' />
 
                             <div className='info-text'>
-                                 <span className='info-span'>민준</span>
-                                 <span className='info-span'>Kangminjun1234</span>
+                                <span className='info-span'>민준</span>
+                                <span className='info-span'>Kangminjun1234</span>
                             </div>
                         </div>
                     </div>
                     <MyInfoIcon
-                         editImg='./icons/mypage/infoedit.png'
-                         editText='정보 수정'
+                        editImg='./icons/mypage/infoedit.png'
+                        editText='정보 수정'
 
-                         festivalImg='./icons/mypage/pick.png'
-                         pickFestivalText='찜 목록'
+                        festivalImg='./icons/mypage/pick.png'
+                        pickFestivalText='찜 목록'
 
-                         artistImg='./icons/mypage/artistedit.png'
-                         pickArtistText='아티스트 수정'
+                        artistImg='./icons/mypage/artistedit.png'
+                        pickArtistText='아티스트 수정'
 
-                         deleteImg='./icons/mypage/deleteuser.png'
-                         deleteInfoText='회원 탈퇴'
+                        deleteImg='./icons/mypage/deleteuser.png'
+                        deleteInfoText='회원 탈퇴'
                     />
-                 </div>
+                </div>
 
 
-                <IconBox 
-                      iconImg1='./icons/mypage/mainpage.png'
-                      iconImg2='./icons/mypage/festivalpage.png'
-                      iconImg3='./icons/mypage/calanderpage.png'
+                <IconBox
+                    iconImg1='./icons/mypage/mainpage.png'
+                    iconImg2='./icons/mypage/festivalpage.png'
+                    iconImg3='./icons/mypage/calanderpage.png'
 
-                      iconText1='메인페이지'
-                      iconText2='페스티벌 페이지'
-                      iconText3='캘린더 페이지'
+                    iconText1='메인페이지'
+                    iconText2='페스티벌 페이지'
+                    iconText3='캘린더 페이지'
 
-                      navLink1={'/home'}
-                      navLink2={'/list'}
-                      navLink3={'/calendar'}
-                   
+                    navLink1={'/'}
+                    navLink2={'/list'}
+                    navLink3={'/calendar'}
+
                 />
 
-                <IconBox 
-                      iconImg1='./icons/mypage/advice.png'
-                      iconImg2='./icons/mypage/myQnA.png'
-                      iconImg3='./icons/mypage/myComment.png'
+                <IconBox
+                    iconImg1='./icons/mypage/advice.png'
+                    iconImg2='./icons/mypage/myQnA.png'
+                    iconImg3='./icons/mypage/myComment.png'
 
-                      iconText1='1:1 문의'
-                      iconText2='내 QnA'
-                      iconText3='내 기대평'
+                    iconText1='1:1 문의'
+                    iconText2='내 QnA'
+                    iconText3='내 기대평'
 
-                      navLink1={'/mydetail/inquiries'}
-                      navLink2={'/mydetail/Qna'}
-                      navLink3={'/mydetail/comments'}
+                    navLink1={'/mydetail/inquiries'}
+                    navLink2={'/mydetail/Qna'}
+                    navLink3={'/mydetail/comments'}
                 />
-               
-                
+
+
                 <div className='ticket-guide-icon'>
                     <div className='ticket-guide'>
-                        <img 
-                              src="./icons/mypage/ticketpage.png" 
-                              alt="티켓 가이드 아이콘" 
-                              className='guide-icon'
+                        <img
+                            src="./icons/mypage/ticketpage.png"
+                            alt="티켓 가이드 아이콘"
+                            className='guide-icon'
                         />
                     </div>
                     <img
-                            src='./icons/mypage/imagechain.png' 
-                            className='chain-image'
+                        src='./icons/mypage/imagechain.png'
+                        className='chain-image'
                     />
-                    
+
                     <TicketBox
                         ticketImg1='./icons/mypage/interpark.png'
                         ticketImg2='./icons/mypage/melonticket.png'
