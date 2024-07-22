da6bc230b4afecfa955e51816765137bfa658671
seo0jjjjj <seo0jjjjj@mail.com>
2024-07-22 17:31:38 +0900

    fix: 순환참조오류 해결

--- src/features/list/component/AllListTab.jsx
+++ src/features/list/component/AllListTab.jsx
@@ -1,145 +1,140 @@
-import React, { useState, useEffect } from 'react';
-import { PosterComponent } from '../index.js';
+import React, { useState, useEffect } from "react";
 
-import '../styles/festival-list.scss'
+import "../styles/festival-list.scss";
+import PosterComponent from "./PosterComponent";
 
 /**
  * 전체 탭 메뉴 모든 컴포넌트
- * @returns 
+ * @returns
  */
 
 const AllListTab = () => {
-
   const [allList, setAllList] = useState([]);
 
   useEffect(() => {
-      
-      const fetchData = async () => {
+    const fetchData = async () => {
+      const data = [
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 1",
+          festivalLocation: "Location 1",
+          festivalDate: "Date 1",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 2",
+          festivalLocation: "Location 2",
+          festivalDate: "Date 2",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 3",
+          festivalLocation: "Location 3",
+          festivalDate: "Date 3",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 4",
+          festivalLocation: "Location 4",
+          festivalDate: "Date 4",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
 
-          const data = [
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 1',
-                        festivalLocation: 'Location 1',
-                        festivalDate: 'Date 1'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 2',
-                        festivalLocation: 'Location 2',
-                        festivalDate: 'Date 2'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 3',
-                        festivalLocation: 'Location 3',
-                        festivalDate: 'Date 3'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 4',
-                        festivalLocation: 'Location 4',
-                        festivalDate: 'Date 4'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
 
-                     {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+        {
+          posterSrc: "",
+          posterAlt: "",
+          festivalTitle: "All Festival Poster 5",
+          festivalLocation: "Location 5",
+          festivalDate: "Date 5",
+        },
+      ];
+      setAllList(data);
+    };
 
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                    {
-                        posterSrc: '',
-                        posterAlt: '',
-                        festivalTitle: 'All Festival Poster 5',
-                        festivalLocation: 'Location 5',
-                        festivalDate: 'Date 5'
-                    },
-                ];
-                setAllList(data);
-            };
+    fetchData();
+  }, []);
 
-            fetchData();
-        }, []);
-
-    return (
-        <div className='festival-list'>
-
-            {allList.map((all, index) => (
-                <PosterComponent 
-                    key={index}
-                    posterSrc={all.posterSrc}
-                    posterAlt={all.posterAlt}
-                    festivalTitle={all.festivalTitle}
-                    festivalLocation={all.festivalLocation}
-                    festivalDate={all.festivalDate}
-                />
-            ))}
-            
-        </div>
-    );
+  return (
+    <div className="festival-list">
+      {allList.map((all, index) => (
+        <PosterComponent
+          key={index}
+          posterSrc={all.posterSrc}
+          posterAlt={all.posterAlt}
+          festivalTitle={all.festivalTitle}
+          festivalLocation={all.festivalLocation}
+          festivalDate={all.festivalDate}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default AllListTab;
---
+++ src/features/register/pages/PrivacyPolicy.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "../styles/infoagreement.css";
+import { RegisterBtn, RegisterCheckbox } from "../component";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="info-agreement-container">
+      <div className="agreement-header">
+        <span className="agreement-header-youth">Youth!</span>
+      </div>
+      <div className="info-agreement-form">
+        <RegisterCheckbox
+          agreementType="checkbox"
+          agreementText="전체 약관 동의"
+        />
+        <RegisterCheckbox
+          agreementType="checkbox"
+          agreementText="[필수]개인 정보 수집 동의"
+        />
+        <p className="register-agreement-notice">
+          Youth!는 서비스 제공을 위하여 아래와 같이 회원의 개인정보를 수집,
+          활용합니다.
+          <br />
+          <br />
+          * 개인정보 수집∙이용에 대한 동의를 거부할 수 있으며, 거부할 경우
+          서비스 이용이 제한됩니다.
+          <br />
+          서비스 이용 과정에서 부정이용방지, 비인가 사용 방지, 통계 및 서비스
+          품질 향상 목적으로 단말기
+          <br />
+          정보(OS, 모델명 등), IP주소, 쿠키, 서비스 이용 기록(방문일시,
+          접속기록) 정보가 자동으로 생성되
+          <br />
+          어 수집될 수 있습니다. 자동으로 생성되어 수집된 정보는 서비스 제공
+          기간동안 보유 및 이용하며,
+          <br />
+          수집 및 이용목적의 달성 시 파기됩니다.
+        </p>
+
+        <RegisterCheckbox
+          agreementType="checkbox"
+          agreementText="[선택]마케팅 수신 동의"
+        />
+        <p className="register-agreement-notice">
+          ㈜Youth! 마케팅 수신 동의 약관
+          <br />
+          <br />
+          [제 1장 총칙]
+          <br />
+          · 1조 목적
+          <br />
+          <span className="register-notice-span">
+            본 약관은 회원(Youth! 서비스 약관에 동의한 마케팅 수신 동의를
+            말하며, 이하 “회원”)이
+          </span>
+          <br />
+          <span className="register-notice-span">
+            주식회사 Youth!(이하 “회사”)가 제공하는 서비스(이하 “서비스”)를
+            이용함에 있어 회사와 회원의 권리/의무 및 책임사항
+          </span>
+          <br />
+          <span className="register-notice-span">
+            을 규정함을 목적으로 합니다.
+          </span>
+          <br />
+          · 제2조 이용약관의 효력 및 변경
+          <br />
+          <span className="register-notice-span">
+            ① 본 약관은 서비스를 신청한 고객이 동의하고 회사가
+          </span>
+        </p>
+
+        <br />
+
+        <RegisterBtn
+          registerBtnText="다음단계"
+          registerNavLink="/register/registerform"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicy;
