7975096acf686a38aa60735b18f09296c4ac3392
moble <dudwls5674@naver.com>
2024-07-23 17:23:46 +0900

    계정 찾기 페이지 수정

--- src/features/userfind/component/IdFindAll.jsx
+++ src/features/userfind/component/IdFindAll.jsx
@@ -9,9 +9,9 @@ const IdFindAll = () => {
         <div>
             <FindToggle 
                 findType='radio' 
-                findText='휴대폰 인증으로 찾기' 
-                toggleContents={<FindBtn findBtnText='휴대폰 인증' findNavLink='/id1'/>} 
-                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 인증으로 아이디를 찾을 수 있어요.'
+                findText='휴대폰 번호로 찾기' 
+                toggleContents={<FindBtn findBtnText='휴대폰 인증' findNavLink='/id3'/>} 
+                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 번호로 아이디를 찾을 수 있어요.'
             />
 
             <FindToggle 
@@ -19,13 +19,6 @@ const IdFindAll = () => {
                 findText='이메일 인증으로 찾기' 
                 toggleContents={<FindBtn findBtnText='이메일 인증' findNavLink='/id2'/>} 
                 toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 이메일 인증으로 아이디를 찾을 수 있어요.'
-            />
-
-            <FindToggle 
-                findType='radio' 
-                findText='휴대폰 번호로 찾기' 
-                toggleContents={<FindBtn findBtnText='휴대폰 인증' findNavLink='/id3'/>} 
-                toggleText='본인확인(실명인증)이 완료된 아이디는 본인명의 휴대폰 번호로 아이디를 찾을 수 있어요.'
             />
         </div>
     );
