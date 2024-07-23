da19dc42665b3b9deab6d942c92d272d0e4d6234
OrangeBoy98 <wogks119@gmail.com>
2024-07-23 15:03:49 +0900

    feat: 카카오 연동 로그인 구현 모듈 추가함 styled, spinner App.js 라우터 추가함

---
+++ src/components/spinner/Spinner.jsx
@@ -0,0 +1,35 @@
+// src/elements/Spinner.js
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const SpinnerWrapper = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  &:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #000;
+    border-color: #000 transparent #000 transparent;
+    animation: ${spin} 1.2s linear infinite;
+  }
+`;
+
+const Spinner = () => (
+  <SpinnerWrapper />
+);
+
+export default Spinner;
