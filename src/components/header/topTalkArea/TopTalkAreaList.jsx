829a6fb194928daa6f6ca5c7f5316352051ab6d6
OrangeBoy98 <wogks119@gmail.com>
2024-07-24 14:43:44 +0900

    feat: 알림 이벤트창 외 다른 영역 클릭시 닫히는 기능 추가

--- src/components/header/topTalkArea/TopTalkAreaList.jsx
+++ src/components/header/topTalkArea/TopTalkAreaList.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import { format } from 'date-fns';
 import './top-talk-area-list.scss'
 
 const TopTalkAreaList = ({ username, event, onEventClick, read }) => {
     const { id, category, content, redirectUrl, createdAt } = event;
+    const formattedDate = format(new Date(createdAt), 'yyyy-MM-dd HH:mm');
     return (
-        <div className='top-talk-area-list-container' style={read ? {} : {color: '#ddd'}}>
+        <div className='top-talk-area-list-container'>
             <div className='header'>
-                <div className='category'> [ {category} ] </div>
-                <div className='event-push'> {createdAt}</div>
+                <div className='category' style={read ? {} : {color: '#ddd'}}> [ {category} ] </div>
+                <div className='event-push'> {formattedDate}</div>
             </div>
-            <div className='content'>
+            <div className='content' style={read ? {} : {color: '#ddd'}}>
                 {username} 님, {content}
             </div>
             <div className='footer' onClick={() => onEventClick(id, redirectUrl)}>
