c7760c8eeebfb6e816a0f63a34b5f03263e3393d
OrangeBoy98 <wogks119@gmail.com>
2024-07-24 11:49:02 +0900

    feat: 알림 스크롤 추가

--- src/components/header/topTalkArea/TopTalkArea.jsx
+++ src/components/header/topTalkArea/TopTalkArea.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import TopTalkAreaList from './TopTalkAreaList';
-import {AuthContext} from '../../../contexts/AuthContext'
+import { AuthContext } from '../../../contexts/AuthContext';
 import axios from 'axios';
 import './top-talk-area.scss';
 
 const apiURL = process.env.REACT_APP_API_URL;
 
 const TopTalkArea = () => {
-    const { user } = React.useContext(AuthContext)
+    const { user } = React.useContext(AuthContext);
     const userId = user.user.userId;
     const [events, setEvents] = useState([]);
 
@@ -17,11 +17,11 @@ const TopTalkArea = () => {
                 const response = await axios.get(`${apiURL}/event/${userId}`);
                 setEvents(response.data.events);
             } catch (error) {
-                console.log(error.response.data.message)
+                console.log(error.response.data.message);
             }
-        }
+        };
         eventData();
-    },[userId]);
+    }, [userId]);
 
     const handleEventClick = async (id, redirectUrl) => {
         try {
@@ -31,24 +31,27 @@ const TopTalkArea = () => {
         } catch (error) {
             console.log(error.response.data.message);
         }
-    }
+    };
+
     return (
-        <div id="topTalkArea" className="talk_area" aria-hidden="false">
+        <div className="talk_area_wrapper">
             <div className="arrow-up"></div>
-            <div className="layer_box">
-                <div className="box_content">
-                {events && events.length > 0 ? (
-                        events.map(event => (
-                            <TopTalkAreaList
-                            username={user.user.username}
-                                key={event.id}
-                                event={event}
-                                onEventClick={handleEventClick}
-                            />
-                        ))
-                    ) : (
-                        <div>No notifications available</div>
-                    )}
+            <div className="talk_area" aria-hidden="false">
+                <div className="layer_box">
+                    <div className="box_content">
+                        {events && events.length > 0 ? (
+                            events.map(event => (
+                                <TopTalkAreaList
+                                    username={user.user.username}
+                                    key={event.id}
+                                    event={event}
+                                    onEventClick={handleEventClick}
+                                />
+                            ))
+                        ) : (
+                            <div>No notifications available</div>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
