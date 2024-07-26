f06416a2899aa5abb387acf17881f5f1eeb79869
moble <dudwls5674@naver.com>
2024-07-26 21:13:03 +0900

    축제 상세 공연장 값 수정

--- src/features/festivalDetail/components/FestivalInfo.jsx
+++ src/features/festivalDetail/components/FestivalInfo.jsx
@@ -99,7 +99,7 @@ const FestivalInfo = ({ festivalId, onScrollToMap }) => {
                         <p className='festival-category'><span className='festival-info-text-category'>장르</span>{festival.category}</p>
                     </div>
                     <div className='festival-additional-info'>
-                        <p className='festival-organizer'><span className='festival-info-text-organizer'>공연장</span><span className='festival-info-value'>{festival.organizer}</span><NextIcon onClick={onScrollToMap} className="scroll-to-map-icon" /></p>
+                        <p className='festival-organizer'><span className='festival-info-text-organizer'>공연장</span><span className='festival-info-value'>{festival?.geoLocation.name}</span><NextIcon onClick={onScrollToMap} className="scroll-to-map-icon" /></p>
                         <p className='festival-min-age'><span className='festival-info-text'>관람등급</span>{formatMinAge(festival.minAge)}</p>
                         <p className='festival-tel'><span className='festival-info-text'>전화번호</span>{festival.tel}</p>
                     </div>
