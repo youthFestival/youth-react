dae9731f4888887f6a94b5b7c13e03641eaf936a
moble <dudwls5674@naver.com>
2024-07-28 22:41:59 +0900

    축제 상세페이지 찜 기능 추가

--- src/features/festivalDetail/components/FestivalLike.jsx
+++ src/features/festivalDetail/components/FestivalLike.jsx
@@ -17,17 +17,19 @@ const FestivalLike = ({ festivalId }) => {
                 const response = await axios.get(`${apiUrl}/festival/${festivalId}/like`, {
                     withCredentials: true,
                 });
-                setLiked(response.data.liked);
-                console.log(response.data.liked);
+
+                if (user && user.userId) {
+                    const isLiked = response.data.favoriteUser.some(favoriteUser => favoriteUser.userId === user.userId);
+                    setLiked(isLiked);
+                }
                 setLikeCount(response.data.likeCount);
-                console.log(response.data.likeCount);
             } catch (error) {
                 console.error('찜 상태를 가져오는데 실패했습니다.', error);
             }
         };
 
         fetchLikeStatus();
-    }, [festivalId]);
+    }, [festivalId, user]);
 
     const onClickLikeBtn = async () => {
         if (!user) {
@@ -35,7 +37,10 @@ const FestivalLike = ({ festivalId }) => {
                 title: '로그인이 필요합니다.',
                 text: '로그인 페이지로 이동합니다.',
                 icon: 'warning',
-                confirmButtonText: '확인'
+                confirmButtonText: '확인',
+                customClass: {
+                    confirmButton: 'custom-confirm-button'
+                }
             }).then(() => {
                 window.location.href = '/login';
             });
@@ -45,7 +50,7 @@ const FestivalLike = ({ festivalId }) => {
         try {
             const apiUrl = process.env.REACT_APP_API_URL;
             const response = await axios.put(`${apiUrl}/festival/${festivalId}/like`, {}, { withCredentials: true });
-            setLiked(response.data.liked);
+            setLiked(response.data.message.includes('취소') ? false : true);
             setLikeCount(response.data.likeCount);
 
             Swal.fire({
@@ -72,12 +77,12 @@ const FestivalLike = ({ festivalId }) => {
 
     return (
         <div className='festival-likes-container'>
-            {liked ? (
+            {liked && user ? (
                 <FavoriteIcon style={{ color: 'red', cursor: 'pointer' }} onClick={onClickLikeBtn} />
             ) : (
                 <FavoriteBorderIcon style={{ color: 'black', cursor: 'pointer' }} onClick={onClickLikeBtn} />
             )}
-            <span>{likeCount}</span>
+            <span className='festival-likeCount'>{likeCount}</span>
         </div>
     );
 };
